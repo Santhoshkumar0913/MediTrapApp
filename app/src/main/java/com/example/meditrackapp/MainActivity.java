18c33c3940d36ed4fdf576369891acaee35c295c
Santhoshkumar0913 <santhoshkumarp2213@gmail.com>
2025-11-03 13:20:15 +0530

    feature: 3/11 auto login.

--- app/src/main/java/com/example/meditrackapp/MainActivity.java
+++ app/src/main/java/com/example/meditrackapp/MainActivity.java
@@ -6,15 +6,18 @@ import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnStart;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); // your welcome/start screen
+        mAuth = FirebaseAuth.getInstance();
 
         btnStart = findViewById(R.id.btnStart); // make sure activity_main.xml has this button
 
@@ -27,5 +30,16 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mAuth != null && mAuth.getCurrentUser() != null) {
+            Intent intent = new Intent(MainActivity.this, Dashboard.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+            finish();
+        }
     }
 }
