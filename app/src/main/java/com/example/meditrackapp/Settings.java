8fac6266d3b886d866a85a6779db0ea705e61082
Santhoshkumar0913 <santhoshkumarp2213@gmail.com>
2025-11-03 13:31:32 +0530

    feature: 3/11 auto login.

--- app/src/main/java/com/example/meditrackapp/Settings.java
+++ app/src/main/java/com/example/meditrackapp/Settings.java
@@ -9,15 +9,12 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class Settings extends BaseActivity {
 
